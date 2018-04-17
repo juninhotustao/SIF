6f7c4f96b319668c3d49d7d3bc5ce521c78b033d
Francisco Junior <juninhotustao@yahoo.com.br>
2018-04-17 13:49:19 -0300

    vamos testar

--- Configura GIT.bat
+++ Configura GIT.bat
@@ -1,6 +1,6 @@
 cd D:\Arquivos Juninho\SIF
 
-git config --global user.name "juninhotustao"
+git config --global user.name "Francisco Junior"
 
 git config --global user.email juninhotustao@yahoo.com.br
 
